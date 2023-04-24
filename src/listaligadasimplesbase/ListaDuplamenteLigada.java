d40fc47e53e472bbbac147380b866c9ce37786e1
yasmin vizeu gil <vizeugilyasmin@gmail.com>
2023-04-24 19:45:34 -0300

    lista duplamente ligada

--- src/listaligadasimplesbase/ListaDuplamenteLigada.java
+++ src/listaligadasimplesbase/ListaDuplamenteLigada.java
@@ -14,12 +14,18 @@ public class ListaDuplamenteLigada {
      * @param args the command line arguments
      */
     static No inicio = null;
-    static No fim = null;
 
     public static void main(String[] args) {
         // TODO code application logic here
-        inserir(5);
-        remover(5);
+        inserirOrdenado(24);
+        inserirOrdenado(4);
+        inserirOrdenado(100);
+        remover(100);
+        removerUltimo();
+        removerPrimeiro();
+        inserirOrdenado(24);
+        inserirOrdenado(4);
+        inserirOrdenado(100);
 
         exibir();
 
@@ -29,11 +35,14 @@ public class ListaDuplamenteLigada {
         No novo = new No(x);
 
         if (inicio == null) {
-            inicio = fim = novo;
+            novo.proximo = novo;
+            novo.anterior = novo;
+            inicio = novo;
         } else {
-            fim.proximo = novo;
-            novo.anterior = fim;
-            fim = novo;
+            novo.proximo = inicio;
+            novo.anterior = inicio.anterior;
+            inicio.anterior.proximo = novo;
+            inicio.anterior = novo;
         }
 
     }
@@ -42,29 +51,34 @@ public class ListaDuplamenteLigada {
         No novo = new No(x);
 
         if (inicio == null) {
-            inicio = fim = novo;
-
-        }
-
-        No temp = inicio;
-
-        while ((temp != null) && (temp.valor < x)) {
-            temp = temp.proximo;
-        }
-
-        if (temp == inicio) {
-            novo.proximo = inicio;
-            inicio.anterior = novo;
+            novo.proximo = novo;
+            novo.anterior = novo;
             inicio = novo;
-        } else if (temp == null) {
-            fim.proximo = novo;
-            novo.anterior = fim;
-            fim = novo;
         } else {
-            novo.proximo = temp;
-            novo.anterior = temp.proximo;
-            temp.anterior.proximo = novo;
-            temp.anterior = novo;
+            //verificar se o valor deve ser encaixado nas extremindades da lista 
+            if ((novo.valor <= inicio.valor) || (novo.valor > inicio.anterior.valor)) {
+                novo.proximo = inicio;
+                novo.anterior = inicio.anterior;
+                inicio.anterior.proximo = novo;
+                inicio.anterior = novo;
+                if (novo.valor > inicio.anterior.valor) {
+                    inicio = novo;
+                }
+            } //verificar se o velor deve ser encaixado no meio da lista
+            else {
+                No temp = inicio.proximo;
+
+                while ((novo.valor > temp.valor) && (temp != inicio)) {
+                    temp = temp.proximo;
+                }
+                if (temp != inicio) {
+                    novo.proximo = temp;
+                    novo.anterior = temp.anterior;
+                    temp.anterior.proximo = novo;
+                    temp.anterior = novo;
+
+                }
+            }
         }
 
     }
@@ -95,38 +109,68 @@ public class ListaDuplamenteLigada {
                 temp = temp.anterior;
             } while (temp != inicio);
 
+            System.out.println(temp.valor);
+
         }
 
     }
 
     public static void remover(int x) {
-        if (inicio == null) {
-            System.out.println("Lista vazia");
-        } else {
-            No temp = inicio;
+        No temp = inicio;
 
-            while ((temp.proximo != null) && (temp.valor != x)) {
-                temp = temp.proximo;
-            }
-
-            if (temp.valor == x) {
-
-                if (temp == temp.proximo) { //verifica se o numero  foi encontrado e for uma lista circular de apenas um elemento
-                    inicio = null;  // remove-se o inicio
-                } else {
-                    temp.anterior.proximo = temp.proximo;
-                    temp.proximo.anterior = temp.anterior;
-
-                    if (temp == inicio) {
-                        inicio = temp.proximo;
-
-                    }
+        while ((temp.valor != x) && (temp.proximo != inicio)) {
+            temp = temp.proximo;
+        }
+        //depois de encontrar o valor de x
+        if (temp.valor == x) {
+            //verificar se a lista é feita de apenas 1 elemento
+            if (temp == temp.proximo) {
+                inicio = null;
+            } //se  alista tiver varios elementos, fazer a remoção do x
+            else {
+                temp.anterior.proximo = temp.proximo;
+                temp.proximo.anterior = temp.anterior;
+                //verificar se a lista chegou ao fim e fazer a modificação dos ponteiros
+                if (temp == inicio) {
+                    inicio = temp.proximo;
                 }
-            } else {// se o numero x nao foi encontrado na lista 
-                System.out.println("Numero nao encontrado");
+            }
+        } else {
+            System.out.println("Valor nao encontrado");
+        }
+
+    }
+
+    public static void removerPrimeiro() {
+        if (inicio == null) {
+            System.out.println("Lista Vazia");
+        } else { //verifica se a lista tem apenas 1 elemento
+            if (inicio == inicio.proximo) {
+                inicio = null;
+            } else {
+                inicio.anterior.proximo = inicio.proximo;
+                inicio.proximo.anterior = inicio.anterior;
+                //ajustar o ponterio inicio
+                inicio = inicio.proximo;
 
             }
 
+        }
+
+    }
+
+    public static void removerUltimo() {
+        if(inicio == null){
+            System.out.println("Lista vazia");
+        }
+        else{
+            if(inicio == inicio.proximo){
+                inicio = null;
+            }
+            else{
+                inicio.anterior.anterior.proximo = inicio;
+                inicio.anterior = inicio.anterior.anterior;
+            }
         }
 
     }
